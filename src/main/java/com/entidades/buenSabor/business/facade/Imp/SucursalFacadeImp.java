a3f5219ab43bc63c0966e19e2f982b5a0558cde2
Joaquin Campana <jncampana99@gmail.com>
2024-05-30 09:56:35 -0300

    A MODIFICAR

---
+++ src/main/java/com/entidades/buenSabor/business/facade/Imp/SucursalFacadeImp.java
@@ -0,0 +1,35 @@
+package com.entidades.buenSabor.business.facade.Imp;
+
+import com.entidades.buenSabor.business.facade.Base.BaseFacadeImp;
+import com.entidades.buenSabor.business.facade.Sucursalfacade;
+import com.entidades.buenSabor.business.mapper.BaseMapper;
+import com.entidades.buenSabor.business.mapper.CategoriaMapper;
+import com.entidades.buenSabor.business.service.Base.BaseService;
+import com.entidades.buenSabor.business.service.SucursalService;
+import com.entidades.buenSabor.domain.dto.Categoria.CategoriaDto;
+import com.entidades.buenSabor.domain.dto.Sucursal.SucursalCreateDto;
+import com.entidades.buenSabor.domain.dto.Sucursal.SucursalDto;
+import com.entidades.buenSabor.domain.dto.Sucursal.SucursalEditDto;
+import com.entidades.buenSabor.domain.entities.Sucursal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SucursalFacadeImp extends BaseFacadeImp<Sucursal, SucursalDto, SucursalCreateDto, SucursalEditDto,Long> implements Sucursalfacade {
+    public SucursalFacadeImp(BaseService<Sucursal, Long> baseService, BaseMapper<Sucursal, SucursalDto, SucursalCreateDto, SucursalEditDto> baseMapper) {
+        super(baseService, baseMapper);
+    }
+
+    @Autowired
+    CategoriaMapper categoriaMapper;
+
+    @Autowired
+    SucursalService sucursalService;
+
+    @Override
+    public List<CategoriaDto> findAllCategoriasByIdSucursal(Long idSucursal) {
+        return categoriaMapper.toDTOsList(sucursalService.findCategoriasBySucursalId(idSucursal));
+    }
+}
